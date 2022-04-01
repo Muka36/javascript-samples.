fe4fa788b4aebc7340c0d0c993632f8b60431799
Muthukarthick T <98314834+Muka36@users.noreply.github.com>
2022-04-01 19:33:00 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,8 @@
+var karthick = `karthick's`
+var muthu = `${karthick} bag`
+
+console.log(karthick);
+console.log (muthu)
+console.log (muthu.toUpperCase())
+console.log (muthu.includes("s"))
+console.log (muthu.endsWith("g"))
